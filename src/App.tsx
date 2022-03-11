af3c6afe58daf14845311c7366767e75b723ae1e
Thomas Ricketts <rickettsdev@gmail.com>
2022-03-11 18:11:09 -0500

    Moved board drawing logic into function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { useEffect, useRef } from 'react'
 
-import logo from './logo.svg';
-import './App.css';
-
 import CoordinateTranslator from './helpers/CoordinateTranslator';
 import RoadsToDraw from './helpers/RoadsToDraw';
-// import getCanvasContext from './canvas/CanvasComponent';
+// import CatanGameBoard from './canvas/CatanGameBoard';
+
+import './App.css';
 
 
 function App() {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  return (
+    <div>
+      <h3>CatanBoardGame</h3>
+      <CatanGameBoard />
+    </div>
+  );
+}
+
+function CatanGameBoard() {
+  const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
 
   const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
 
-  useEffect(() => {
+  useEffect(() => { 
     // Initialize
     if (canvasRef.current) {
       canvasCtxRef.current = canvasRef.current.getContext('2d');
@@ -25,24 +33,22 @@ function App() {
       for (var road of RoadsToDraw.roadsCoordinates()/*.filter(rd => rd.y == 0)*/) {
         let uiCoordinates = CoordinateTranslator.uiCoordinateMap({x: road.x, y: road.y, x1: road.x1 , y1: road.y1});
 
-
-         ctx!.beginPath(); // Note the Non Null Assertion
-         ctx!.moveTo(uiCoordinates.x, uiCoordinates.y);
-          ctx!.lineTo(uiCoordinates.x1, uiCoordinates.y1);
-          ctx!.strokeStyle = color;
-          ctx!.lineWidth = width;
-         ctx!.stroke();
+        ctx!.beginPath(); // Note the Non Null Assertion
+        ctx!.moveTo(uiCoordinates.x, uiCoordinates.y);
+        ctx!.lineTo(uiCoordinates.x1, uiCoordinates.y1);
+        ctx!.strokeStyle = color;
+        ctx!.lineWidth = width;
+        ctx!.stroke();
       }
       
     }
   }, []);
- 
-  const coordinates = JSON.stringify(CoordinateTranslator.uiCoordinateMap({ x: 0, y: 1, x1: 0, y1: 2 }));
 
-  for (var roads of RoadsToDraw.roadsCoordinates().filter(rd => rd.y == 0)) {
+  for (var roads of RoadsToDraw.roadsCoordinates().filter(rd => rd.y === 0)) {
     console.log(JSON.stringify(CoordinateTranslator.uiCoordinateMap(roads)));
   }
   return (
+    <div>
       <canvas
         ref={canvasRef}
         width={500}
@@ -51,6 +57,7 @@ function App() {
           console.log(`click on x: ${e.clientX}, y: ${e.clientY}`);
         }}
       />
+    </div>
   );
 }
 
